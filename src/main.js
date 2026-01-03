1e5f42ad671b31631ba39e3699ae92401065ddfd
Abel <malebochester@gmail.com>
2026-01-03 19:35:16 +0200

    bug patch attempt ->  001

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // src/main.js
 
-import "./core-modules/auth-module/auth.js"; // auth bootstraps first
+import "./lib/modules/auth-module/auth.js"; // auth bootstraps first
 import initRouter from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
