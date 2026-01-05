adee44461160ba76a2cc653d6676458b4e75b972
Abel <malebochester@gmail.com>
2026-01-05 15:16:43 +0200

    update pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // src/main.js
 
-import "./lib/modules/auth-module/auth.js"; // auth bootstraps first
+import "./modules/auth-module/auth.js"; // auth bootstraps first
 import initRouter from "./router.js";
 
 document.addEventListener("DOMContentLoaded", () => {
